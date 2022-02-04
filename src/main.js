fe5a9c43e1be4fbd468f5dbf771817aa3f26d124
O-Marsters-1997 <olly.marsters@yahoo.co.uk>
2022-02-04 19:26:00 +0000

    getPrimes function returns an array of length n when n is used as an input

--- src/main.js
+++ src/main.js
@@ -1,10 +1,33 @@
-const readline = require("readline");
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+const getUserInput = () => {
+  const readline = require("readline");
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
 
-rl.question("Please choose a number!", function(number){
-    console.log(`returing the first ${number} prime numbers`)
-    process.exit(0)
-})
+  rl.question("Please choose a number!", function (number) {
+    console.log(`returing the first ${number} prime numbers`);
+    process.exit(0);
+  });
+};
+
+function getPrimes(num) {
+  let primes = [];
+  for (let i = 2; primes.length < num; i++) {
+    let divisibleNum = false;
+    for (let count = 2; count < i; count++) {
+      if (i % count === 0) {
+        divisibleNum = true;
+        break;
+      }
+    }
+    if (divisibleNum == false) {
+      primes.push(i);
+    }
+  }
+  return primes;
+}
+
+console.log(getPrimes(5));
+
+module.exports = getPrimes
--- src/main.test.js
+++ src/main.test.js
@@ -1,2 +1,7 @@
 const getPrimes = require('./main')
 
+describe('getPrimes', ()=> {
+    it('should return an array of equal lenth to the number that is inputted', ()=> {
+        expect(getPrimes(4).length).toBe(4)
+    })
+})
